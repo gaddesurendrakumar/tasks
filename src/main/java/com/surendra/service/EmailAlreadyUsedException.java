5ca7f9eb4a218717d4d98c3be80b02c2a34c1128
sgadde <gaddesurendrakumar@gmail.com>
2020-02-22 13:25:47 +0530

    Initial version of tasks generated by JHipster-6.4.1

---
+++ src/main/java/com/surendra/service/EmailAlreadyUsedException.java
@@ -0,0 +1,9 @@
+package com.surendra.service;
+
+public class EmailAlreadyUsedException extends RuntimeException {
+
+    public EmailAlreadyUsedException() {
+        super("Email is already in use!");
+    }
+
+}
